aa1b94e2bc8f6d209269351a111b838d43d913d0
Mika Halla <mikahalla@gmail.com>
2023-05-17 17:20:23 +0200

    create tweet in database done, 55:00

---
+++ src/components/SideNav.tsx
@@ -0,0 +1,29 @@
+import { signIn, signOut, useSession } from "next-auth/react";
+import Link from "next/link";
+
+const SideNav = () => {
+  const session = useSession();
+  const user = session.data?.user;
+  return (
+    <nav className="sticky top-0 self-start px-2 py-4">
+      <ul className="flex flex-col items-start gap-2 whitespace-nowrap">
+        <li>
+          <Link href={"/"}>Home</Link>
+        </li>
+        {user != null ? (
+          <li>
+            <Link href={`/profiles/${user.id}`}>Profile</Link>
+          </li>
+        ) : null}
+        <li>
+          {user == null ? (
+            <button onClick={() => void signIn()}>Log In</button>
+          ) : (
+            <button onClick={() => void signOut()}>Log Out</button>
+          )}
+        </li>
+      </ul>
+    </nav>
+  );
+};
+export default SideNav;
---
+++ src/pages/profiles/[id].tsx
@@ -0,0 +1,10 @@
+import Link from "next/link";
+
+const id = () => {
+  return (
+    <div>
+      <Link href={"/"}>Back to main</Link>
+    </div>
+  );
+};
+export default id;
